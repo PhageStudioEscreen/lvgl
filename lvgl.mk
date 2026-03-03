5ea00f712c844da67b7d44355e460614590997aa
egahp <egahp.@qq.com>
2026-03-03 16:43:21 +0800

    feat: remove useless src
    Signed-off-by: egahp <egahp.@qq.com>

--- lvgl.mk
+++ lvgl.mk
@@ -1,9 +1,17 @@
 LVGL_PATH ?= ${shell pwd}/lvgl
+LVGL_ENABLE_DEMOS ?= 0
+LVGL_ENABLE_EXAMPLES ?= 0
 
 ASRCS += $(shell find $(LVGL_PATH)/src -type f -name '*.S')
 CSRCS += $(shell find $(LVGL_PATH)/src -type f -name '*.c')
+
+ifeq ($(LVGL_ENABLE_DEMOS), 1)
 CSRCS += $(shell find $(LVGL_PATH)/demos -type f -name '*.c')
+endif
+
+ifeq ($(LVGL_ENABLE_EXAMPLES), 1)
 CSRCS += $(shell find $(LVGL_PATH)/examples -type f -name '*.c')
+endif
 CXXEXT := .cpp
 CXXSRCS += $(shell find $(LVGL_PATH)/src -type f -name '*${CXXEXT}')
 
